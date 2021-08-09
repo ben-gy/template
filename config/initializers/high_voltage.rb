b81e2c2474acb64d82e483260fec0290d1c3991c
Ben Richardson <hi@ben.gy>
2021-08-09 19:02:51 +1000

    Update high_voltage.rb

--- config/initializers/high_voltage.rb
+++ config/initializers/high_voltage.rb
@@ -1,5 +1,13 @@
 HighVoltage.configure do |config|
+  # configure homepage
   config.home_page = 'home'
+
+  # enable naked pages
   config.route_drawer = HighVoltage::RouteDrawers::Root
+
+  # apply a layout to all pages
   config.layout = 'site'
+
+  # use pages controller
+  # config.routes = false
 end
